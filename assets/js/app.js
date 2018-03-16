ac14a03438b87cbab30f744bdb524e2457482990
rodrigosrising <rodrigosrising@gmail.com>
2018-03-16 16:39:15 -0300

    arquivos do site Clinica Ilumina

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$('.hero').slick({
+  autoplay: true,
+  dots: true,
+  infinite: true,
+  speed: 800,
+  slidesToShow: 1,
+  fade: true,
+  cssEase: 'linear',
+  adaptiveHeight: true,
+});
+
+$(document).foundation();
